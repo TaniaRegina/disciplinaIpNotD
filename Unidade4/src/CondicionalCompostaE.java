63e4b4e06a886bca94196384f5d80362bde7d96b
robsonsouza10 <67963178+robsonsouza10@users.noreply.github.com>
2023-04-03 19:33:47 -0300

    Fiz em aula
    Fiz em aula

---
+++ Unidade4/src/CondicionalCompostaE.java
@@ -0,0 +1,11 @@
+public class CondicionalCompostaE {
+  public static void main(String[] args) {
+     int idade = 16;
+     
+     if ((idade >= 18) && (idade < 60)){
+        System.out.println("Pagante");
+     }
+
+     System.out.println("..FIM..");
+  }  
+}
---
+++ Unidade4/src/CondicionalCompostaOU.java
@@ -0,0 +1,12 @@
+public class CondicionalCompostaOU {
+public static void main(String[] args) {
+    int idade = 18;
+    
+    if ( idade < 18 || idade >= 60){
+            System.out.println("Meia Entrada");
+    }
+
+    System.out.println("..FIM..");
+}
+    
+}
